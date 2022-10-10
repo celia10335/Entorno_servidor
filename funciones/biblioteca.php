69c0d24c75d3476846d18bb9d9cb64279a0faa6d
celia <10335@cifpceuta.es>
2022-10-10 14:10:36 +0200

    new file:   funciones/biblioteca.php 	deleted:    funciones_avanzadas.php 	deleted:    funciones_probando.php 	deleted:    funciones_sencillas.php 	funciones/arrayFunciones.php 	funciones/funciones_avanzadas.php 	funciones/funciones_sencillas.php

---
+++ funciones/biblioteca.php
@@ -0,0 +1,43 @@
+<?php
+function suma(...$num){
+    if (count($num) == 0){
+        return false;
+    } else {
+        $suma = 0;
+        foreach($num as $n){
+            $suma+=$n;
+        }
+        return $suma;
+    }
+}
+
+function resta(){
+    if (func_num_args() == 0){
+        return false;
+    } else {
+        $resta = func_get_arg(0);
+        for ($i=1; $i<func_num_args(); $i++){
+            $resta -= func_get_arg($i);
+        }
+        return $resta;
+    }
+}
+
+function multiplica(...$num){
+    if (count($num) == 0){
+        return false;
+    } else {
+        $producto = 1;
+        foreach($num as $n){
+            $producto *= $n;
+        }
+    }
+}
+
+function divide($a, $b){
+    return (int)($a/$b);
+}
+
+
+
+?>
