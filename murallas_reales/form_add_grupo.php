604e5ff36bf39d8c2abfc1c69cd755822063a43a
celia <10335@cifpceuta.es>
2022-11-03 14:12:13 +0100

    práctica Murallas Reales 	new file:   murallas_reales/form_add_alumno.php 	new file:   murallas_reales/form_add_grupo.php 	new file:   murallas_reales/form_add_profesor.php

---
+++ murallas_reales/form_add_grupo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario entrada datos</title>
+</head>
+<body>
+
+<p>Introduzca los datos del grupo</p>
+<form action="POST">
+<p>
+    <label for="idgrupo">Id</label>
+    <input type="number" name="idgrupo">
+</p>
+<p>
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre">
+</p>
+<p>
+    <label for="curso">Curso</label>
+    <input type="text" name="curso">
+</p>
+<p>
+    <input type="submit" value="Insertar">
+</p>
+
+</form>
+    
+</body>
+</html>
---
+++ murallas_reales/form_add_profesor.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario entrada datos</title>
+</head>
+<body>
+
+<p>Introduzca los datos del alumno</p>
+<form action="POST">
+<p>
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre">
+</p>
+<p>
+    <label for="apellidos">Apellidos</label>
+    <input type="text" name="apellidos">
+</p>
+<p>
+    <label for="grupo">Grupo</label>
+    <input type="number" name="grupo">
+</p>
+<p>
+    <label for="expediente">Expediente</label>
+    <input type="number" name="expediente">
+</p>
+<p>
+    <label for="telefono">Teléfono</label>
+    <input type="number" name="telefono">
+</p>
+<p>
+    <label for="email">E-mail</label>
+    <input type="email" name="email">
+</p>
+<p>
+    <input type="submit" value="Insertar">
+</p>
+
+</form>
+    
+</body>
+</html>
