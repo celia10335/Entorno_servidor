e6b5f341298807de443bd9aac23bb65018119292
celia <10335@cifpceuta.es>
2022-11-08 09:39:54 +0100

    parte2 	modified:   murallas_reales/form_add_alumno.php 	modified:   murallas_reales/inserta_alumno.php

--- murallas_reales/form_add_alumno.php
+++ murallas_reales/form_add_alumno.php
@@ -1,44 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario entrada datos</title>
 </head>
+
 <body>
 
-<p>Introduzca los datos del alumno</p>
-<form action="inserta_alumno.php" method="POST">
-<p>
-    <label for="nombre">Nombre</label>
-    <input type="text" name="nombre">
-</p>
-<p>
-    <label for="apellidos">Apellidos</label>
-    <input type="text" name="apellidos">
-</p>
-<p>
-    <label for="grupo">Grupo</label>
-    <input type="text" name="grupo">
-</p>
-<p>
-    <label for="expediente">Expediente</label>
-    <input type="number" name="expediente">
-</p>
-<p>
-    <label for="telefono">Teléfono</label>
-    <input type="text" name="telefono">
-</p>
-<p>
-    <label for="email">E-mail</label>
-    <input type="email" name="email">
-</p>
-<p>
-    <input type="submit" value="Insertar">
-</p>
+<?php
+include_once "./conectar.php";
 
-</form>
-    
+
+$sql = "SELECT id_grupo, nombre FROM Grupo";
+
+$resultado = $conexion->query($sql);
+
+
+?>
+
+    <p>Introduzca los datos del alumno</p>
+    <form action="inserta_alumno.php" method="POST">
+        <p>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre">
+        </p>
+        <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+        </p>
+        <p>
+            <label for="idgrupo">Grupo</label>
+            <select name="idgrupo">
+
+            <?php
+            if ($resultado->num_rows > 0) {
+                while ($row = $resultado->fetch_assoc()) {
+                    $id = $row["id_grupo"];
+                    $nombre = $row["nombre"];
+                    echo "<option value='$id'>$nombre</option>";
+                }
+            }           
+            ?>
+
+            </select>
+        </p>
+        <p>
+            <label for="expediente">Expediente</label>
+            <input type="number" name="expediente">
+        </p>
+        <p>
+            <label for="telefono">Teléfono</label>
+            <input type="text" name="telefono">
+        </p>
+        <p>
+            <label for="email">E-mail</label>
+            <input type="email" name="email">
+        </p>
+        <p>
+            <input type="submit" value="Insertar">
+        </p>
+
+    </form>
+
+    <?php
+
+
+    $conexion->close();
+    ?>
+
 </body>
+
 </html>
--- murallas_reales/inserta_alumno.php
+++ murallas_reales/inserta_alumno.php
@@ -1,8 +1,22 @@
 <?php
-include_once "./conectar.php";
 
-$sql = "INSERT INTO Alumno (nombre, apellidos, expediente, telefono, email)
-VALUES ('$_POST[nombre]', '$_POST[apellidos]', '$_POST[expediente]', '$_POST[telefono]', '$_POST[email]')";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "murallas_reales";
+
+
+// Conectar
+$conexion = new mysqli($servername, $username, $password, $dbname);
+
+// Comprobar conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: ".$conexion->connect_error);
+}
+
+
+$sql = "INSERT INTO Alumno (id_grupo, nombre, apellidos, expediente, telefono, email)
+VALUES ('$_POST[idgrupo]', '$_POST[nombre]', '$_POST[apellidos]', '$_POST[expediente]', '$_POST[telefono]', '$_POST[email]')";
 
 if ($conexion->query($sql) === true){
     echo "Nuevo registro insertado";
