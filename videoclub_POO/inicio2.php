3ae2e4fceab3c27199bb8e950dfab4731b1d0914
celia <10335@cifpceuta.es>
2022-11-24 12:59:08 +0100

    envio 	new file:   videoclub_POO/inicio2.php

---
+++ videoclub_POO/inicio2.php
@@ -0,0 +1,13 @@
+<?php
+
+include './CintaVideo.php';
+
+$miCinta = new CintaVideo("Los cazafantasmas", 22, 3.5, 107);
+
+echo "<strong>".$miCinta->titulo."</strong>";
+echo "<br>Precio: ".$miCinta->getPrecio()." euros";
+echo "<br>Precio IVA incluido: ".$miCinta->getPrecioConIva()." euros";
+echo "<br>";
+$miCinta->muestraResumen();
+
+?>
