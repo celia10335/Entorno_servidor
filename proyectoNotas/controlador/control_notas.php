613c694635378251a6055f22f73d59ac27f0f4ca
celia <10335@cifpceuta.es>
2022-12-13 08:41:20 +0100

    . 	modified:   proyectoNotas/controlador/control_notas.php

--- proyectoNotas/controlador/control_notas.php
+++ proyectoNotas/controlador/control_notas.php
@@ -1,18 +1,26 @@
 <?php
 
 class Controlador{
-    public $vista = "list";
-    public $objetoTabla = new NoteTable();
+    public $vista;
+    public $titulo_pag;
+    public $objetoNotas;
 
 
     public function __construct(){
-
+        public $vista = "list";
+        public $titulo_pag = "";
+        public $objetoNotas = new NoteTable();
     }
 
 
     public function list(){
-        $this->vista = "list";
-        $this->objetoTabla->getNotes();
+        $this->titulo_pag = "Listado de notas";
+        return $this->objetoNotas->getNotes();
+    }
+
+    public function insert(){
+        $this->vista = "insert";
+        $this->titulo_pag = "Crear nota";
     }
 
     public function edit(){
