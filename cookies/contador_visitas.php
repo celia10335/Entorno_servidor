c40055f6a4697d7e4ad47b5c86b4c3147b76f061
celia <10335@cifpceuta.es>
2023-01-13 13:59:06 +0100

    13-enero 	modified:   cookies/contador_visitas.php 	modified:   cookies/reiniciar_contador.php

--- cookies/contador_visitas.php
+++ cookies/contador_visitas.php
@@ -1,22 +1,29 @@
 <?php
+// Inicializo la visita en 1 y creo la cookie
 $visitas = 1;
 setcookie("contador", $visitas);
 
 
+// Si la cookie está creada, la variable "visitas" se incrementa en 1 cada vez que se recargue la página. Este valor se almacena en la cookie
 if (isset($_COOKIE["contador"])) {
     $visitas = $_COOKIE["contador"] + 1;
     setcookie("contador", $visitas);
 
+// El mensaje que devuelve la página es el valor almacenado en la cookie desde la recarga anterior. Por eso, para que sea real, tengo que incrementarlo en 1. El valor actual no lo leerá el navegador hasta que se abandone la página
     echo "Contador: visitaste esta página " . $_COOKIE["contador"] + 1 . " veces";
-} else {
 
+
+
+} else {
+// Si no hay cookie aún por ser la primera visita, el mensaje será diferente ("es la primera visita...")    
     echo "Contador: es la primera vez que visitas esta página";
 }
 
-echo "<p>Pulsa aquí para reiniciar el contador</p>";
 
+echo "<p>Pulsa aquí para reiniciar el contador</p>";
 ?>
 
+<!-- El enlace lleva a otro php para poner el contador a cero -->
 <div
     style="height: 24px; width: 215px; border: solid 1px grey; margin-top: 20px; padding:10px; background-color:white; font-size:1.1em">
     <a href="./reiniciar_contador.php">Reiniciar</a>
--- cookies/reiniciar_contador.php
+++ cookies/reiniciar_contador.php
@@ -1,5 +1,5 @@
 <?php
-
+// Desde este php se reinicia el contador y redirige a la página principal. Lo hice así, de forma separada, porque si no, cada vez que recargaba la página el contador se iniciaba en 1.
 setcookie("contador", $visitas, 1);
 header('Location: contador_visitas.php');
 ?>
